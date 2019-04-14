573f5b37cae5f876d2fcbafe341cac617116b105
raju <govindh410@gmail.com>
2019-04-14 09:18:19 +0530

    first commit

---
+++ src/main/java/com/example/demo/WebserviceExample1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceExample1Application.class, args);
+	}
+
+}
